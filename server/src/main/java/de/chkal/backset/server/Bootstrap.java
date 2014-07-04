96b6c27422d233fd13cee55ee4671fd4a481ecec
Christian Kaltepoth <christian@kaltepoth.de>
2014-07-04 17:21:18 +0200

    Added Logback as the prefered logging implementation

--- server/src/main/java/de/chkal/backset/server/Bootstrap.java
+++ server/src/main/java/de/chkal/backset/server/Bootstrap.java
@@ -1,10 +1,15 @@
 package de.chkal.backset.server;
 
+import org.slf4j.bridge.SLF4JBridgeHandler;
+
 import de.chkal.backset.core.Backset;
 
 public class Bootstrap {
 
   public static void main(String[] args) {
+
+    SLF4JBridgeHandler.removeHandlersForRootLogger();
+    SLF4JBridgeHandler.install();
 
     Backset backset = Backset.builder().build();
     backset.start();
