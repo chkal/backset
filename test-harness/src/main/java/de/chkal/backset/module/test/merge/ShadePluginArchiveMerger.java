d66897c6c88a479b5f18537dd6e0319ab824a282
Christian Kaltepoth <christian@kaltepoth.de>
2014-07-04 08:04:02 +0200

    Added ShadePluginArchiveMerger to replace SimpleArchiveMerger

---
+++ test-harness/src/main/java/de/chkal/backset/module/test/merge/ShadePluginArchiveMerger.java
@@ -0,0 +1,120 @@
+package de.chkal.backset.module.test.merge;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.UUID;
+
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.shade.DefaultShader;
+import org.apache.maven.plugins.shade.ShadeRequest;
+import org.apache.maven.plugins.shade.filter.Filter;
+import org.apache.maven.plugins.shade.relocation.Relocator;
+import org.apache.maven.plugins.shade.resource.ResourceTransformer;
+import org.apache.maven.plugins.shade.resource.ServicesResourceTransformer;
+import org.codehaus.plexus.logging.console.ConsoleLogger;
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.exporter.ZipExporter;
+import org.jboss.shrinkwrap.api.importer.ZipImporter;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+
+public class ShadePluginArchiveMerger {
+
+  private JavaArchive base;
+
+  private final List<Archive<?>> archives = new ArrayList<>();
+
+  private final List<File> tempFiles = new ArrayList<>();
+
+  private ShadePluginArchiveMerger(JavaArchive base) {
+    this.base = base;
+  }
+
+  public static ShadePluginArchiveMerger create(JavaArchive base) {
+    return new ShadePluginArchiveMerger(base);
+  }
+
+  public ShadePluginArchiveMerger merge(Iterable<Archive<?>> archives) {
+    for (Archive<?> archive : archives) {
+      merge(archive);
+    }
+    return this;
+  }
+
+  public ShadePluginArchiveMerger merge(Archive<?>[] archives) {
+    for (Archive<?> archive : archives) {
+      merge(archive);
+    }
+    return this;
+  }
+
+  public ShadePluginArchiveMerger merge(Archive<?> other) {
+    archives.add(other);
+    return this;
+  }
+
+  public JavaArchive getResult() {
+
+    try {
+
+      Set<File> files = new LinkedHashSet<>();
+
+      files.add(asFile(base));
+
+      for (Archive<?> archive : archives) {
+        files.add(asFile(archive));
+      }
+
+      File uberJar = createTempFile();
+
+      ShadeRequest request = new ShadeRequest();
+      request.setUberJar(uberJar);
+      request.setJars(files);
+      request.setFilters(new ArrayList<Filter>());
+      request.setRelocators(new ArrayList<Relocator>());
+      request.setShadeSourcesContent(false);
+      request.setResourceTransformers(getDefaultResourceTransformers());
+
+      DefaultShader shader = new DefaultShader();
+      shader.enableLogging(new ConsoleLogger());
+      shader.shade(request);
+
+      return ShrinkWrap.create(ZipImporter.class, base.getName())
+          .importFrom(uberJar)
+          .as(JavaArchive.class);
+
+    } catch (MojoExecutionException | IOException e) {
+      throw new IllegalStateException("Failed", e);
+    }
+
+  }
+
+  private List<ResourceTransformer> getDefaultResourceTransformers() {
+
+    ResourceTransformer servicesTransformer = new ServicesResourceTransformer();
+
+    return Arrays.asList(servicesTransformer);
+
+  }
+
+  private File asFile(Archive<?> archive) {
+    File file = createTempFile();
+    archive.as(ZipExporter.class).exportTo(file);
+    return file;
+  }
+
+  private File createTempFile() {
+    String tmpDir = System.getProperty("java.io.tmpdir");
+    String jarFile = UUID.randomUUID().toString() + ".jar";
+    File file = Paths.get(tmpDir, jarFile).toFile();
+    tempFiles.add(file);
+    return file;
+  }
+
+}
---
+++ test-harness/src/test/java/de/chkal/backset/module/test/merge/ShadePluginArchiveMergerTest.java
@@ -0,0 +1,101 @@
+package de.chkal.backset.module.test.merge;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.IOException;
+
+import org.apache.commons.io.IOUtils;
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.Node;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.StringAsset;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Test;
+
+public class ShadePluginArchiveMergerTest {
+
+  @Test
+  public void mergeDifferentFilesIntoOneArchive() {
+
+    JavaArchive first = ShrinkWrap.create(JavaArchive.class);
+    first.add(new StringAsset("foo"), "first.txt");
+
+    JavaArchive second = ShrinkWrap.create(JavaArchive.class);
+    second.add(new StringAsset("bar"), "second.txt");
+
+    JavaArchive result = ShadePluginArchiveMerger.create(first).merge(second).getResult();
+
+    assertThat(result.getContent()).hasSize(2);
+
+    assertThat(getAsString(result, "first.txt")).isEqualTo("foo");
+    assertThat(getAsString(result, "second.txt")).isEqualTo("bar");
+
+  }
+
+  @Test
+  public void mergeDirectories() {
+
+    JavaArchive first = ShrinkWrap.create(JavaArchive.class);
+    first.add(new StringAsset("foo"), "first.txt");
+
+    JavaArchive second = ShrinkWrap.create(JavaArchive.class);
+    second.add(new StringAsset("bar"), "second.txt");
+
+    JavaArchive result = ShadePluginArchiveMerger.create(first).merge(second).getResult();
+
+    assertThat(result.getContent()).hasSize(2);
+
+    assertThat(getAsString(result, "first.txt")).isEqualTo("foo");
+    assertThat(getAsString(result, "second.txt")).isEqualTo("bar");
+
+  }
+  
+  @Test
+  public void mergeByOverwriting() {
+
+    JavaArchive first = ShrinkWrap.create(JavaArchive.class);
+    first.add(new StringAsset("foo"), "directory/foo.txt");
+
+    JavaArchive second = ShrinkWrap.create(JavaArchive.class);
+    second.add(new StringAsset("bar"), "directory/bar.txt");
+
+    JavaArchive result = ShadePluginArchiveMerger.create(first).merge(second).getResult();
+
+    assertThat(result.getContent()).hasSize(3);
+
+    assertThat(getAsString(result, "directory/foo.txt")).contains("foo");
+    assertThat(getAsString(result, "directory/bar.txt")).contains("bar");
+
+  }
+
+  @Test
+  public void mergeServiceProviderFiles() {
+
+    JavaArchive first = ShrinkWrap.create(JavaArchive.class);
+    first.add(new StringAsset("FirstImpl"), "META-INF/services/MyInterface");
+
+    JavaArchive second = ShrinkWrap.create(JavaArchive.class);
+    second.add(new StringAsset("SecondImpl"), "META-INF/services/MyInterface");
+
+    JavaArchive result = ShadePluginArchiveMerger.create(first).merge(second).getResult();
+
+    assertThat(getAsString(result, "META-INF/services/MyInterface"))
+        .contains("FirstImpl")
+        .contains("SecondImpl")
+        .isEqualTo("FirstImpl\nSecondImpl");
+
+  }
+
+  private String getAsString(Archive<?> archive, String path) {
+    try {
+      Node node = archive.get(path);
+      if (node != null && node.getAsset() != null) {
+        return IOUtils.toString(node.getAsset().openStream());
+      }
+      return null;
+    } catch (IOException e) {
+      throw new IllegalStateException(e);
+    }
+  }
+
+}
