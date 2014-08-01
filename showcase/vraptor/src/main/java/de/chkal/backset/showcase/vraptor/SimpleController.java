46d8afb011fe8f650007c51621b10ead5a5a0626
Christian Kaltepoth <christian@kaltepoth.de>
2014-08-01 17:10:16 +0200

    First steps for a VRaptor module

---
+++ showcase/vraptor/src/main/java/de/chkal/backset/showcase/vraptor/SimpleController.java
@@ -0,0 +1,14 @@
+package de.chkal.backset.showcase.vraptor;
+
+import br.com.caelum.vraptor.Controller;
+import br.com.caelum.vraptor.Get;
+
+@Controller
+public class SimpleController {
+
+  @Get("/index")
+  public void get() {
+
+  }
+
+}
