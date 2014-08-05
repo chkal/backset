5488601fe994a9ca7a12c0c5e5ca4742729bbef7
Christian Kaltepoth <christian@kaltepoth.de>
2014-08-05 08:14:07 +0200

    Some more work for the VRaptor showcase

--- showcase/vraptor/src/main/java/de/chkal/backset/showcase/vraptor/SimpleController.java
+++ showcase/vraptor/src/main/java/de/chkal/backset/showcase/vraptor/SimpleController.java
@@ -1,13 +1,22 @@
 package de.chkal.backset.showcase.vraptor;
 
+import javax.inject.Inject;
+
 import br.com.caelum.vraptor.Controller;
 import br.com.caelum.vraptor.Get;
+import br.com.caelum.vraptor.Result;
 
 @Controller
 public class SimpleController {
 
+  @Inject
+  private Result result;
+
   @Get("/index")
   public void get() {
+
+    result.include("foo", "bar");
+    result.forwardTo("/test.jsp");
 
   }
 
